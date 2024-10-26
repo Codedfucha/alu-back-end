3fddb80ac4c43430a0e02339e8c39ceed9caf8c9
Codedfucha <a.gichure@alustudent.com>
2024-10-26 18:56:04 +0000

    again

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-fetches data from an API
-and exports it to a JSON file
+Fetches data from an API
+and exports it to a JSON file.
 """
 
 import json
@@ -11,18 +11,27 @@ from sys import argv
 if __name__ == '__main__':
     userId = argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
+                        format(userId)).json()
+
+    # Check if user exists
+    if not user or 'id' not in user or user['id'] != int(userId):
+        print("User not found.")
+        exit(1)
+
     todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
+                        format(userId)).json()
+
     username = user.get('username')
     tasks = []
     for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
+        task_dict = {
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+        }
         tasks.append(task_dict)
-    jsonobj = {}
-    jsonobj[userId] = tasks
+
+    jsonobj = {userId: tasks}
+
     with open("{}.json".format(userId), 'w') as jsonfile:
         json.dump(jsonobj, jsonfile)
